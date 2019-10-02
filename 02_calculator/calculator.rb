bf93db0ef6009bb033ca4b15e4e4502e3ea9a235
Nathaniel Perrin <nperrin@thepointmag.com>
2019-10-02 14:41:53 -0500

    Exercises 1-3

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,33 @@
 #write your code here
+
+def add(first, second)
+  first + second
+end
+
+def subtract(first, second)
+  first - second
+end
+
+def sum(array)
+  array.inject(0){|sum,x| sum + x }
+end
+
+def multiply(first, second)
+  first * second
+end
+
+def power(first, second)
+  first ** second
+end
+
+def factorial(num)
+  if num = 0
+    1
+  end
+else
+  i = num
+  while i > 0
+    i - 1
+    num * i
+  end
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,33 @@
 #write your code here
+
+def echo(word)
+  word
+end
+
+def shout(word)
+  word.upcase
+end
+
+def repeat(word, n = 2)
+    n.times.collect { word }.join(" ")
+end
+
+def start_of_word(word, num=1)
+  word[0, num]
+end
+
+def first_word(word)
+  word.split(" ").first
+end
+
+def titleize(word)
+  words = word.split.map do |word|
+  if %w(the and over).include?(word)
+    word
+  else
+    word.capitalize
+  end
+end
+words.first.capitalize!
+words.join(" ")
+end
